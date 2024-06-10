fe363112b5b173c20d63581f072a1e4b51711639
peyoot <peyoot@hotmail.com>
2024-06-10 01:28:26 -0500

    synx: fix packing issue for ccmp

--- tools/publish.sh
+++ tools/publish.sh
@@ -356,7 +356,7 @@ if prompt-yesno "Scripts will copy major images to release folder, continue?" ye
     sync
     sleep 2
     if [[ "${ISROS}" == "yes" ]]; then
-      find "${DEST_PATH}" -type f \( -name 'dey-image-qtros*' -o -name 'install_*' -o -name 'imx*' -name 'tf*' -name 'fip*' -name 'u-boot*' -o -name 'boot.scr' \) -a \( ! -name '*.zip' \) -exec zip -j "${DEST_PATH}/${PROJECT}_sd_installer.zip" {} +
+      find "${DEST_PATH}" -type f \( -name 'dey-image-qtros*' -o -name 'install_*' -o -name 'imx*' -o -name 'tf*' -o -name 'fip*' -o -name 'u-boot*' -o -name 'boot.scr' \) -a \( ! -name '*.zip' \) -exec zip -j "${DEST_PATH}/${PROJECT}_sd_installer.zip" {} +
 #    zip -j ${DEST_PATH}/${PROJECT}_sd_installer.zip ${DEST_PATH}/* -x ${DEST_PATH}/${PROJECT}_sd_installer.zip
     else
       find "${DEST_PATH}" -type f \( -name "${IMAGE}*" -o -name 'install_*' -o -name 'imx*' -o -name 'tf*' -o -name 'fip*' -o -name 'u-boot*' -o -name 'boot.scr' \) -a \( ! -name '*.zip' ! -name 'dey-image-qtros*' ! -name '*humble*' \) -exec zip -j "${DEST_PATH}/${PROJECT}_sd_installer.zip" {} +
