6ac005f9cbd0b71115e9749b7c4bdbfbd01503e6
peyoot <peyoot@hotmail.com>
2024-10-22 06:36:54 +0000

    fix failing to copy dtb issue

--- tools/publish.sh
+++ tools/publish.sh
@@ -359,7 +359,7 @@ if prompt-yesno "Scripts will copy major images to release folder, continue?" ye
     cp ${SRC_UBOOT}/${UBOOT_FILE} ${DEST_PATH}/
     cp ${SRC_UBOOT}/install_linux* ${DEST_PATH}/
     cp ${SRC_UBOOT}/boot.scr ${DEST_PATH}/
-    cp ${SRC_DTB} ${DEST_PATH}/dtb/
+    cp ${SRC_DTB}/*.dtb ${DEST_PATH}/dtb/
     if [[ "${PLATFORM}" =~ "mp" ]] ; then
       echo "copy ST platform bootloader"
       cp ${SRC_BASE}/tf-a-${PLATFORM}-*.stm32 ${DEST_PATH}/
