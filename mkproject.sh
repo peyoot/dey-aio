434b2dc741429f82bbe1ac1b28d27c74ce4df4e2
peyoot <peyoot@hotmail.com>
2026-05-28 23:39:42 -0500

    update

--- mkproject.sh
+++ mkproject.sh
@@ -152,7 +152,7 @@ do_mkproject() {
 	if [ -z "${MKP_OLD_PROJECT}" ]; then
 		# Customize project
 		chmod 644 ${MKP_PROJECTPATH}/conf/bblayers.conf ${MKP_PROJECTPATH}/conf/local.conf
-		sed -i -e 's,  ##DIGIBASE##/meta-st-x-linux-ai \\,  ##DIGIBASE##/meta-digi/meta-st-x-linux-ai \\\n  ##DIGIBASE##/meta-st-x-linux-isp \\,g' ${MKP_PROJECTPATH}/conf/bblayers.conf
+		sed -i -e 's,  ##DIGIBASE##/meta-st-x-linux-ai \\,  ##DIGIBASE##/meta-st-x-linux-ai \\\n  ##DIGIBASE##/meta-st-x-linux-isp \\,g' ${MKP_PROJECTPATH}/conf/bblayers.conf
 		sed -i -e 's,  ##DIGIBASE##/meta-digi/meta-digi-dey \\,  ##DIGIBASE##/meta-digi/meta-digi-dey \\\n  ##DIGIBASE##/meta-custom \\,g' ${MKP_PROJECTPATH}/conf/bblayers.conf
 		sed -i -e "s,##DIGIBASE##,${MKP_SCRIPTPATH}/sources,g" ${MKP_PROJECTPATH}/conf/bblayers.conf
 		# At this point the user has accepted all the licenses, so enable the vendor EULA
